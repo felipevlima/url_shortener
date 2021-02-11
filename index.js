48a291255763af38cd0c1cbfa8510a63dcc4c275
Felipe Lima <felip.3lima@hotmail.com>
2021-02-11 14:20:45 -0300

    Change: inital commit

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 const shortUrlRoute = require("./routes/shorturl")
 const getShortenUrlRoute = require("./routes/getShortenurl")
+const cors = require('cors');
+
 const app = express();
 connectDB();
+
 const PORT = process.env.PORT;
+app.use(cors());
 app.use(express.json({}));
 app.listen(PORT|| 3000, () => console.log(`Server is listening on port ${PORT}`));
 
