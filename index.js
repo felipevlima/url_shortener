1651a3e8cb6934b7881836ce2f72de153477ac6f
Felipe Lima <felip.3lima@hotmail.com>
2021-02-11 14:27:53 -0300

    Change: inital commit

--- index.js
+++ index.js
@@ -8,10 +8,9 @@ const cors = require('cors');
 const app = express();
 connectDB();
 
-const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json({}));
-app.listen(PORT|| 3000, () => console.log(`Server is listening on port ${PORT}`));
 
 app.use("/",getShortenUrlRoute)
 app.use("/shorturl", shortUrlRoute);
+app.listen(3000, () => console.log(`Server is listening on port 3000`));
